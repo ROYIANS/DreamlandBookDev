4f3b3893f2babf39d5512e3d920dbc27b7bc3835
ROYIANS <1294686101@qq.com>
2023-03-15 17:25:16 +0800

    1

---
+++ dreamland-book/source/_posts/categories.md
@@ -0,0 +1,9 @@
+title: Categories
+date: 2013-12-24 23:30:09
+categories:
+- 个人思考
+- 学习笔记
+- 旅行日记
+---
+
+This post contains 3 categories. Make sure your theme can display all of the categories.
---
+++ dreamland-book/themes/dreamland-book/scripts/helpers/page.js
@@ -0,0 +1,66 @@
+/**
+ * Dreamland Book
+ * @example
+ */
+
+'use strict'
+
+const { stripHTML, escapeHTML, prettyUrls } = require('hexo-util')
+
+hexo.extend.helper.register('page_description', function () {
+  const { config, page } = this
+  let description = page.description || page.content || page.title || config.description
+
+  if (description) {
+    description = escapeHTML(stripHTML(description).substring(0, 150)
+      .trim()
+    ).replace(/\n/g, ' ')
+    return description
+  }
+})
+
+hexo.extend.helper.register('injectHtml', function (data) {
+  let result = ''
+  if (!data) return ''
+  for (let i = 0; i < data.length; i++) {
+    result += data[i]
+  }
+  return result
+})
+
+hexo.extend.helper.register('findArchivesTitle', function (page, menu, date) {
+  if (page.year) {
+    const dateStr = page.month ? `${page.year}-${page.month}` : `${page.year}`
+    const dateFormat = page.month ? hexo.theme.config.aside.card_archives.format : 'YYYY'
+    return date(dateStr, dateFormat)
+  }
+
+  const defaultTitle = this._p('page.archives')
+  if (!menu) return defaultTitle
+
+  const loop = (m) => {
+    for (const key in m) {
+      if (typeof m[key] === 'object') {
+        loop(m[key])
+      }
+
+      if (/\/archives\//.test(m[key])) {
+        return key
+      }
+    }
+  }
+
+  return loop(menu) || defaultTitle
+})
+
+hexo.extend.helper.register('isImgOrUrl', function (path) {
+  const imgTestReg = /\.(png|jpe?g|gif|svg|webp)(\?.*)?$/i
+  if (path.indexOf('//') !== -1 || imgTestReg.test(path)) {
+    return true
+  }
+  return false
+})
+
+hexo.extend.helper.register('urlNoIndex', function (url = null, trailingIndex = false, trailingHtml = false) {
+  return prettyUrls(url || this.url, { trailing_index: trailingIndex, trailing_html: trailingHtml })
+})
